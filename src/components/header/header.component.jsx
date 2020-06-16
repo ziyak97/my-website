a1716375ed8d106185ad66e0551612ae02dfce96
Ziyak Jehangir <ziyak97@gmail.com>
2020-06-16 11:16:12 +0530

    Fixed email spelling.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,7 +14,7 @@ const Header = () => {
                 <h1 className={styles.name__main}>Ziyak Jehangir.</h1>
                 <h2 className={styles.name__secondary}>I build stuff for the web.</h2>
                 <p className={styles.details}>I'm a software engineer based in Mumbai, India. I specializing in building high quality websites and everything in between. Let me know if there is something I can build for you.</p>
-                <div className={styles.button}><a href="mailto:ziyak97@gamil.com">Get In Touch</a></div>
+                <div className={styles.button}><a href="mailto:ziyak97@gmail.com">Get In Touch</a></div>
             </ScrollAnimation>
         </header>
 
